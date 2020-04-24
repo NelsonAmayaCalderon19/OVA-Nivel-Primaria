0312b34151a260885c639a123a215710a1655ad2
NelsonAmaya19 <nelsoneduardoac@ufps.edu.co>
2020-04-24 11:46:00 -0500

    Menjorando el Contenido 4 de Secundaria

--- build/web/js/contenido_secundaria3.js
+++ build/web/js/contenido_secundaria3.js
@@ -2,14 +2,12 @@
 $(document).ready(function() {
     setTimeout(function() {
         $("#contenido").fadeIn(500);
-    },500); 
-    setTimeout(function() {
         $("#camp1").fadeIn(1500);
         $("#camp3").fadeIn(1500);
-    },1500);  
+    },500); 
     setTimeout(function() {
         $("#camp2").fadeIn(1500);
-    },5500); 
+    },15000); 
 });
                
 
