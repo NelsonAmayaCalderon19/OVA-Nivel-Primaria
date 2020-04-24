1c20aa58965ba54020e9a53a0c59ceb4b7efd864
NelsonAmaya19 <nelsoneduardoac@ufps.edu.co>
2020-04-24 16:15:28 -0500

    Aplicando js

---
+++ build/web/js/contenido_secundaria13.js
@@ -0,0 +1,20 @@
+             
+$(document).ready(function() {
+    setTimeout(function() {
+        $("#contenido").fadeIn(1000);
+    },500);
+    setTimeout(function() {
+        $("#conten").fadeIn(1000);
+    },1000);
+    setTimeout(function() {
+        $("#conten2").fadeIn(1000);
+    },2000);
+//    setTimeout(function() {
+//        $("#conten3").fadeIn(1000);
+//    },3000);
+//    setTimeout(function() {
+//        $("#conten4").fadeIn(1000);
+//    },4000);
+});
+
+
