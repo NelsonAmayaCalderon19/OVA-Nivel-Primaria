b2a0503e22c63e4051646a700a45d4d7539000cb
NelsonAmaya19 <nelsoneduardoac@ufps.edu.co>
2020-05-06 14:20:03 -0500

    Añadiendo BD

--- ova.sql
+++ ova.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-05-2020 a las 04:03:46
+-- Tiempo de generación: 06-05-2020 a las 20:42:22
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.2
 
@@ -32,6 +32,7 @@ CREATE TABLE `alumno` (
   `identificacion` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
   `nombre` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
   `id_tipo` int(11) NOT NULL,
+  `id_Colegio` int(11) NOT NULL,
   `fecha` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
@@ -39,31 +40,31 @@ CREATE TABLE `alumno` (
 -- Volcado de datos para la tabla `alumno`
 --
 
-INSERT INTO `alumno` (`identificacion`, `nombre`, `id_tipo`, `fecha`) VALUES
-('1093798043', 'Nelson Amaya', 2, '2020-04-25 19:33:50'),
-('1293934734', 'Andres Rojas', 2, '2020-04-26 09:47:46'),
-('21454654854', 'Nelson Amaya', 2, '2020-04-27 16:21:35'),
-('23442334', 'Jose Orozco', 1, '2020-04-25 20:19:00'),
-('3435345345', 'Andres Duran', 1, '2020-04-29 21:47:52'),
-('3454365', 'Fercho Ortiz', 1, '2020-04-26 09:51:08'),
-('3534534534', 'Nelson Caicedo', 2, '2020-05-01 10:16:23'),
-('353465436', 'Daniel Andrade', 1, '2020-04-29 21:35:12'),
-('3544326432', 'Pedro Arias', 1, '2020-04-25 20:08:31'),
-('423142314123', 'Alberto Diaz', 1, '2020-04-29 21:25:10'),
-('4234123324', 'Stella Calderon', 2, '2020-04-25 20:16:05'),
-('4325435342', 'Amparo', 1, '2020-04-29 21:39:30'),
-('433252345', 'Antonio Cervantes', 1, '2020-04-29 21:41:03'),
-('4341324', 'Fercho Pedraza', 1, '2020-04-29 21:26:43'),
-('4362432645', 'Jaider Amaya', 1, '2020-04-26 09:52:57'),
-('53532562', 'Adriana Rojas', 2, '2020-04-26 09:49:23'),
-('535435432534', 'Ana Navarro', 1, '2020-04-29 21:42:36'),
-('53643643', 'Jose Padilla', 1, '2020-04-26 09:56:03'),
-('537374656', 'Carlos Blanco', 1, '2020-04-26 09:54:56'),
-('543524534', 'Ana Castillo', 2, '2020-04-26 09:56:58'),
-('56464364', 'Pedro Camaño', 1, '2020-04-29 21:45:49'),
-('65464564554', 'Estaquia Ordoñez', 1, '2020-04-29 21:44:23'),
-('6546456456', 'Joselito Uruquijo', 1, '2020-04-29 21:37:52'),
-('65465373', 'Geovanny Calderon', 2, '2020-04-26 09:48:32');
+INSERT INTO `alumno` (`identificacion`, `nombre`, `id_tipo`, `id_Colegio`, `fecha`) VALUES
+('1093798043', 'Nelson Amaya', 2, 1, '2020-04-25 19:33:50'),
+('1293934734', 'Andres Rojas', 2, 2, '2020-04-26 09:47:46'),
+('21454654854', 'Nelson Amaya', 2, 4, '2020-04-27 16:21:35'),
+('23442334', 'Jose Orozco', 1, 1, '2020-04-25 20:19:00'),
+('3435345345', 'Andres Duran', 1, 3, '2020-04-29 21:47:52'),
+('3454365', 'Fercho Ortiz', 1, 2, '2020-04-26 09:51:08'),
+('3534534534', 'Nelson Caicedo', 2, 1, '2020-05-01 10:16:23'),
+('353465436', 'Daniel Andrade', 1, 1, '2020-04-29 21:35:12'),
+('3544326432', 'Pedro Arias', 1, 3, '2020-04-25 20:08:31'),
+('423142314123', 'Alberto Diaz', 1, 4, '2020-04-29 21:25:10'),
+('4234123324', 'Stella Calderon', 2, 1, '2020-04-25 20:16:05'),
+('4325435342', 'Amparo', 1, 2, '2020-04-29 21:39:30'),
+('433252345', 'Antonio Cervantes', 1, 2, '2020-04-29 21:41:03'),
+('4341324', 'Fercho Pedraza', 1, 3, '2020-04-29 21:26:43'),
+('4362432645', 'Jaider Amaya', 1, 4, '2020-04-26 09:52:57'),
+('53532562', 'Adriana Rojas', 2, 2, '2020-04-26 09:49:23'),
+('535435432534', 'Ana Navarro', 1, 1, '2020-04-29 21:42:36'),
+('53643643', 'Jose Padilla', 1, 4, '2020-04-26 09:56:03'),
+('537374656', 'Carlos Blanco', 1, 3, '2020-04-26 09:54:56'),
+('543524534', 'Ana Castillo', 2, 3, '2020-04-26 09:56:58'),
+('56464364', 'Pedro Camaño', 1, 2, '2020-04-29 21:45:49'),
+('65464564554', 'Estaquia Ordoñez', 1, 1, '2020-04-29 21:44:23'),
+('6546456456', 'Joselito Uruquijo', 1, 2, '2020-04-29 21:37:52'),
+('65465373', 'Geovanny Calderon', 2, 3, '2020-04-26 09:48:32');
 
 -- --------------------------------------------------------
 
@@ -261,59 +262,6 @@ INSERT INTO `pregunta` (`id_cuestionario`, `id`, `descripcion`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `preg_opc`
---
-
-CREATE TABLE `preg_opc` (
-  `id_cuestionario` int(11) NOT NULL,
-  `id_pregunta` int(11) NOT NULL,
-  `opc1` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `opc2` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `opc3` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `opc4` varchar(200) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `preg_opc`
---
-
-INSERT INTO `preg_opc` (`id_cuestionario`, `id_pregunta`, `opc1`, `opc2`, `opc3`, `opc4`) VALUES
-(1, 1, 'Reducir', 'Reutilizar', 'Respetar', 'Reciclar'),
-(1, 2, 'Papel, Vidrio, Cartón', 'Espejos, Lentes, Cristales', 'Desechos de Comida', 'Ninguno de los Anteriores'),
-(1, 3, 'Amarillo', 'Verde', 'Negro', 'Azul'),
-(1, 4, 'Azul', 'Amarillo', 'Verde', 'Otro'),
-(1, 5, 'Plantar un Árbol', 'Reciclando', 'Reducir la cantidad de residuos que produces', 'Todo lo Anterior'),
-(1, 6, 'Tus Profesores', 'Todas las Personas', 'Tu mejor Amigo', 'Tus Padres'),
-(1, 7, 'Diez Veces', 'Una Vez', 'Para Siempre', 'Tres Veces'),
-(1, 8, 'Una Bombilla', 'Objetos de Cristal', 'Cerámica o Porcelana', 'Todos los Anteriores'),
-(1, 9, 'Azul', 'Amarillo', 'Verde', 'Gris'),
-(1, 10, 'Reciclar', 'Reducir', 'Reparar', 'Reutilizar'),
-(1, 11, 'Residuos Orgánicos', 'Vidrio', 'Plastico', 'Icopor'),
-(1, 12, 'Papel y Cartón', 'Madera', 'Plástico', 'Vidrio'),
-(1, 13, '1 y 29%', '30 y 55%', '56 y 78% ', '79 y 100%'),
-(1, 14, 'Papel', 'Textiles-Ropa', 'Comida', 'Ninguno de los anteriores'),
-(1, 15, '700 años aproximadamente', '20 años o menos', '100 años exactamente', ' más de 1000 años'),
-(1, 16, 'Dona los articulos que yan no usas', 'Utiliza el papel por ambas caras', 'Compra articulos con poco papel envolvente', 'Busca de una forma creativa dar uso a envases que tienes en tu casa'),
-(2, 1, 'Rojo', 'Blanco', 'Azul', 'Gris'),
-(2, 2, 'El Entorno Físico', 'Las Áreas Verdes', 'Todo aquello que nos rodea', 'Ninguna de las Anteriores'),
-(2, 3, 'Gris', 'Verde', 'Amarillo', 'Otro'),
-(2, 4, 'Papel,Vidrios,Plástico', 'Papel Fotografico,Materia Orgánica,Aluminio', 'Lentes,Pañales,Espejos', 'Todos los Anteriores'),
-(2, 5, 'Rojo', 'Azul', 'Verde', 'Gris'),
-(2, 6, 'Gris', 'Negro', 'Rojo', 'Blanco'),
-(2, 7, '90% de esos desechos', '70% de esos desechos', '20% de esos desechos', '100% de esos desechos'),
-(2, 8, 'Pilas y Baterias', 'Papel y Cartón', 'Pinturas y aceites', 'Ninguna de las Anteriores'),
-(2, 9, '9 árboles', '12 árboles', '5 árboles', '17 árboles'),
-(2, 10, '1 Tonelada', '2 Toneladas', '1,5 Toneladas', 'Menos de 1 Tonelada'),
-(2, 11, 'Amarillo', 'Verde', 'Rojo', 'Otro Distinto'),
-(2, 12, 'Menos de 1 Año', 'Más de 100 Años', 'Entre 70 y 80 años', 'Nunca de Descompone'),
-(2, 13, 'Plástico', 'Cartón', 'Vidrio', 'Poliestireno'),
-(2, 14, '51 por ciento', '43 por ciento', '78 por ciento', '30 por ciento'),
-(2, 15, 'Evitamos la emisión de 1 kilogramo de CO2 a la atmósfera', 'Reducimos 1 kilogramo de basura que va al vertedero', 'Ahorramos más de 1 kilogramo de materia prima', 'Todas las respuestas anteriores son correctas'),
-(2, 16, 'Reducir - Reutilizar - Reciclar', 'Reciclar - Reducir - Reutilizar', 'Reducir - Reciclar - Reutilizar', 'Reutilizar - Reducir - Reciclar');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `puntuacion`
 --
 
@@ -396,7 +344,8 @@ INSERT INTO `tipo_documento` (`id`, `descripcion`) VALUES
 --
 ALTER TABLE `alumno`
   ADD PRIMARY KEY (`identificacion`,`id_tipo`) USING BTREE,
-  ADD KEY `f_tipo` (`id_tipo`);
+  ADD KEY `f_tipo` (`id_tipo`),
+  ADD KEY `id_Colegio` (`id_Colegio`);
 
 --
 -- Indices de la tabla `certificado`
@@ -436,12 +385,6 @@ ALTER TABLE `nivel`
 --
 ALTER TABLE `pregunta`
   ADD PRIMARY KEY (`id_cuestionario`,`id`);
-
---
--- Indices de la tabla `preg_opc`
---
-ALTER TABLE `preg_opc`
-  ADD PRIMARY KEY (`id_cuestionario`,`id_pregunta`);
 
 --
 -- Indices de la tabla `puntuacion`
@@ -497,6 +440,7 @@ ALTER TABLE `tipo_documento`
 -- Filtros para la tabla `alumno`
 --
 ALTER TABLE `alumno`
+  ADD CONSTRAINT `alumno_ibfk_1` FOREIGN KEY (`id_Colegio`) REFERENCES `colegio` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `f_tipo` FOREIGN KEY (`id_tipo`) REFERENCES `tipo_documento` (`id`) ON UPDATE CASCADE;
 
 --
@@ -518,12 +462,6 @@ ALTER TABLE `cuestionario`
 --
 ALTER TABLE `pregunta`
   ADD CONSTRAINT `fk_cuestion` FOREIGN KEY (`id_cuestionario`) REFERENCES `cuestionario` (`id_cuestionario`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `preg_opc`
---
-ALTER TABLE `preg_opc`
-  ADD CONSTRAINT `fkey_cuestionario` FOREIGN KEY (`id_cuestionario`) REFERENCES `pregunta` (`id_cuestionario`) ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `puntuacion`
