e8a174978eb242e7b776f641c8cb85b3c91ed953
NelsonAmaya19 <nelsoneduardoac@ufps.edu.co>
2020-05-06 14:27:58 -0500

    Proyecto Actualizado

--- src/README.md
+++
@@ -1,2 +0,0 @@
-# OVA-Nivel-Primaria
-Objeto Virtual de Aprendizaje para estudiantes de preescola, primaria y secundaria, implementada con JSP Servlets.
--- src/java/modelo/Cuestion.java
+++ src/java/modelo/Cuestion.java
@@ -22,7 +22,7 @@ public class Cuestion {
     int r;
     
     
-     public ResultSet listarTodos(){
+    public ResultSet listarTodos(){
     String sql=("select pun.id_cuestionario, p.descripcion,pun.id_pregunta,pun.opc,pun.opc2,pun.opc3,pun.opc4,pun.opc,pun.puntuacion,pun.puntuacion2,pun.puntuacion3,pun.puntuacion4 FROM pregunta p, puntuacion pun where p.id=pun.id_pregunta and pun.id_cuestionario=p.id_cuestionario and p.id_cuestionario=1 GROUP BY pun.id_pregunta order by rand() limit 10");   
          try {
              con=cn.Conexion();
